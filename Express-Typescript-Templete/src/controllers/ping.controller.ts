d85cd664e0e0f437d8b7e53c0b72cd839b4304c4
Anurag07-07 <adityanv8@gmail.com>
2026-09-15 09:28:03 +0530

    Error Handlng Implemented

--- Express-Typescript-Templete/src/controllers/ping.controller.ts
+++ Express-Typescript-Templete/src/controllers/ping.controller.ts
@@ -1,7 +1,14 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
+import { InternalServerError } from "../utils/errors/app.error.js";
+import fs from 'fs/promises'
 
-export const pingHandler = (req:Request,res:Response)=>{
-  res.send("Pong")
+export const pingHandler = async(req:Request,res:Response,next:NextFunction)=>{
+  try {
+    await fs.readFile("sample")
+    res.status(200).json({message:`Pong`})    
+  } catch (error) {
+    throw new InternalServerError("Something Went Wrong!!!")
+  }
 }
 
 export const systemHealth = (req:Request,res:Response)=>{
--- Express-Typescript-Templete/src/server.ts
+++ Express-Typescript-Templete/src/server.ts
@@ -2,12 +2,17 @@ import express from 'express'
 import { serverConfig } from './config/index.js';
 import v1Router from './routers/v1/index.router.js';
 import v2Router from './routers/v2/index.router.js';
+import { genericErrorHandler } from './middlewares/error.middleware.js';
 
 const app = express();
 app.use(express.json())
 
 app.get('/api/v1',v1Router)
 app.get('/api/v2',v2Router)
+
+
+//Error Middleware
+app.use(genericErrorHandler)
 
 app.listen(serverConfig.PORT,()=>{
   console.log(`Server Running at http://localhost:${serverConfig.PORT}`);
