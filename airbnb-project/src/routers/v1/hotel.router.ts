49ffaf17f56ddf308226b18c023e04454f23551f
Anurag07-07 <adityanv8@gmail.com>
2026-09-18 12:29:27 +0530

    everything work fine

--- airbnb-project/src/routers/v1/hotel.router.ts
+++ airbnb-project/src/routers/v1/hotel.router.ts
@@ -5,8 +5,8 @@ import { hotelSchema } from '../../validators/hotel.validator';
 
 const hotelRouter = express.Router();
 
-hotelRouter.get('/', validateRequestBody(hotelSchema), createHotelHandler); // TODO: Resolve this TS compilation issue
-hotelRouter.post('/:id', getHotelByIdHandler); // TODO: Resolve this TS compilation issue
+hotelRouter.post('/', validateRequestBody(hotelSchema), createHotelHandler); // TODO: Resolve this TS compilation issue
+hotelRouter.get('/:id', getHotelByIdHandler); // TODO: Resolve this TS compilation issue
 
 hotelRouter.get('/health', (req, res) => {
     res.status(200).send('OK');
