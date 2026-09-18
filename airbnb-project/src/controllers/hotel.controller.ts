44a76bc228f16872f74a95bf83872772e1886a1a
Anurag07-07 <adityanv8@gmail.com>
2026-09-18 12:48:42 +0530

    Hotel Create Service Working Fine

--- airbnb-project/src/controllers/hotel.controller.ts
+++ airbnb-project/src/controllers/hotel.controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { createHotelService, getHotelByIdService } from "../services/hotel.service";
-
+import {StatusCodes} from 'http-status-codes'
 export async function createHotelHandler(req:Request,res:Response,next:NextFunction) {
   const hotelResponse = await createHotelService(req.body)
 
-  res.status(201).json({
+  res.status(StatusCodes.CREATED).json({
     message:`Hotel Created Succesfully`,
     data:hotelResponse,
     success:true
@@ -14,10 +14,15 @@ export async function createHotelHandler(req:Request,res:Response,next:NextFunct
 export async function getHotelByIdHandler(req:Request,res:Response,next:NextFunction) {
   const hotelResponse = await getHotelByIdService(Number(req.params.id))
 
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     message:`Hotel found Successfully`,
     data:hotelResponse,
     success:true
   })
 }
 
+
+
+export async function getAllHotelHandler(req:Request,res:Response,next:NextFunction) {
+  res.status(StatusCodes.NOT_IMPLEMENTED);
+}
